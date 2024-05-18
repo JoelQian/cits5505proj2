2207753c9e76331dfbdf8f72e9f33b4a95c3cb5d
David-Shang <146736310+David-Shang@users.noreply.github.com>
2024-05-18 22:10:54 +0800

    add unit test function (#38)
    * feat: testing functions
    
    * fix: change database
    
    * fix: change init.py
    
    * fix: flask env config
    
    * fix: env config

--- app/__init__.py
+++ app/__init__.py
@@ -14,10 +14,18 @@ if WIN:
 else:
     prefix = 'sqlite:////'
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(basedir, 'app.db')
+
+# 设置测试模式
+app.config['TESTING'] = True  # 在测试时设置为True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'dev'
-db = SQLAlchemy(app)
+
+# 设置数据库URI
+if app.config['TESTING']:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'  # 使用内存数据库
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(basedir, 'app.db')
+
 
 login_manager = LoginManager(app)
 # login_manager.login_view = 'index'
